c557cdf930992da155d52ed0d5143909a053fdf3
★. KuJu -TH- <plan291037@outlook.com>
2022-07-01 18:13:55 +0700

    cheage intents discord

--- index.js
+++ index.js
@@ -1,29 +1,5 @@
   const Discord = require("discord.js");
-  const bot = new Discord.Client({
-   intents : [
-     Discord.Intents.FLAGS.GUILDS ,
-     Discord.Intents.FLAGS.GUILD_MEMBERS ,
-     Discord.Intents.FLAGS.GUILD_BANS ,
-     Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS ,
-     Discord.Intents.FLAGS.GUILD_INTEGRATIONS ,
-     Discord.Intents.FLAGS.GUILD_WEBHOOKS ,
-     Discord.Intents.FLAGS.GUILD_INVITES ,
-     Discord.Intents.FLAGS.GUILD_VOICE_STATES ,
-     Discord.Intents.FLAGS.GUILD_PRESENCES ,
-     Discord.Intents.FLAGS.GUILD_MESSAGES ,
-     Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS ,
-     Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING ,
-     Discord.Intents.FLAGS.DIRECT_MESSAGES , 
-     Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS ,
-     Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING ,
-    ],
-    allowedMentions: {
-        parse: ["everyone", "roles", "users"],
-        repliedUser: true
-    },
-    partials: ["CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION", "USER"]
-
-  });
+  const bot = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MEMBERS, Discord.Intents.FLAGS.DIRECT_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGES] });
 
   const config = require('./config.json');
   const inFo = require('./server/info.js');
