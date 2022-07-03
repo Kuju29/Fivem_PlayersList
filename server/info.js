91af712846e17452278a9aba7e57e0e39f7d7d2c
★. KuJu -TH- <plan291037@outlook.com>
2022-07-03 07:56:15 +0700

    beautify json

--- server/info.js
+++ server/info.js
@@ -1,19 +1,19 @@
 const fetch = require('@vercel/fetch')(require('node-fetch'));
 
 class ApiFiveM {
-    constructor(ip) {
-        if (!ip) throw Error('Please put "IP:Port"');
-        this.ip = ip;
-    }
+  constructor(ip) {
+    if (!ip) throw Error('Please put "IP:Port"');
+    this.ip = ip;
+  }
 
   async checkOnlineStatus() {
 
     try {
-        const online = await fetch(`http://${this.ip}/dynamic.json`);
-        return online.status >= 200 && online.status < 300;
-      } catch (err) {
-        return false;
-      }
+      const online = await fetch(`http://${this.ip}/dynamic.json`);
+      return online.status >= 200 && online.status < 300;
+    } catch (err) {
+      return false;
+    }
   };
 
   async getDynamic() {
@@ -22,22 +22,22 @@ class ApiFiveM {
     const data = await res.json();
 
     if (res.ok) {
-        return data;
-      } else {
-        return null;
-      }
+      return data;
+    } else {
+      return null;
+    }
   };
 
   async getPlayers() {
 
     const res = await fetch(`http://${this.ip}/players.json`);
     const data = await res.json();
-  
+
     if (res.ok) {
-        return data;
-      } else {
-        return null;
-      }
+      return data;
+    } else {
+      return null;
+    }
   };
 };
 
