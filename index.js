e6fb1d22cf2c76d30dead59c1e0515f5d8c6261c
★. KuJu -TH- <plan291037@outlook.com>
2022-07-08 17:53:42 +0700

    remove !start

--- index.js
+++ index.js
@@ -2,18 +2,15 @@ const Discord = require('discord.js');
 const bot = new Discord.Client({
   intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MEMBERS, Discord.Intents.FLAGS.DIRECT_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGES]
 });
+bot.commands = new Discord.Collection();
 
 const config = require('./config.json');
 const fivem = require('./server/info.js');
-const {
-  Pagination
-} = require("discordjs-button-embed-pagination");
+const fs = require('fs');
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 
 const SERVER_NAME = config.SERVER_NAME;
 const BOT_TOKEN = config.BOT_TOKEN;
-const SERVER_LOGO = config.SERVER_LOGO;
-const CHANNEL_ID = config.CHANNEL_ID;
-const COLORBOX = config.COLORBOX;
 const NAMELIST = config.NAMELIST;
 const NAMELISTENABLE = config.NAMELISTENABLE;
 const AUTODELETE = config.AUTODELETE;
@@ -36,7 +33,6 @@ console.log = function (data) {
 
 const activity = async () => {
   inFo.checkOnlineStatus().then(async (server) => {
-    let channel = bot.channels.cache.get(CHANNEL_ID);
     if (server) {
       let players = (await inFo.getPlayers());
       let playersonline = (await inFo.getDynamic()).clients;
@@ -44,22 +40,6 @@ const activity = async () => {
       let namef = players.filter(function (person) {
         return person.name.toLowerCase().includes(NAMELIST);
       });
-      if (STATUS !== "Online" && CHANNEL_ID !== "") {
-          let embed = new Discord.MessageEmbed()
-            .setColor(COLORBOX)
-            .setThumbnail(SERVER_LOGO)
-            .setTitle(SERVER_NAME)
-            .setDescription(`Server Status : **Online** 🟢\nTag : `)
-            .setTimestamp(new Date());
-          channel.send({
-            embeds: [embed]
-          }).then(async (message) => {
-            STATUS = "Online";
-            console.log('Send Online message done');
-          }).catch((err) => {
-            console.log('Check "your channel ID"' + err);
-          });
-      }
 
       if (playersonline === 0) {
         bot.user.setActivity(`⚠ Wait for Connect`, {
@@ -85,50 +65,12 @@ const activity = async () => {
         'type': 'WATCHING'
       });
       console.log(`Offline at activity`);
-      if (STATUS !== null && CHANNEL_ID !== "") {
-          let embed = new Discord.MessageEmbed()
-            .setColor(COLORBOX)
-            .setThumbnail(SERVER_LOGO)
-            .setTitle(SERVER_NAME)
-            .setDescription(`Server Status : **Offline** 🔴\nTag : `)
-            .setTimestamp(new Date());
-          channel.send({
-            embeds: [embed]
-          }).then(async (message) => {
-            STATUS = null;
-            console.log('Send Offline message done');
-          }).catch((err) => {
-            console.log('Check "your channel ID"' + err);
-          });
-      }
     }
 
   }).catch((err) => {
     console.log(`Catch ERROR` + err);
   });
 };
-
-function splitChunks(sourceArray, chunkSize) {
-  let result = [];
-  for (var i = 0; i < sourceArray.length; i += chunkSize) {
-    result[i / chunkSize] = sourceArray.slice(i, i + chunkSize);
-  }
-  return result;
-};
-
-function validateIpAndPort(input) {
-  var parts = input.split(":");
-  var ip = parts[0].split(".");
-  var port = parts[1];
-  return validateNum(port, 1, 65535) && ip.length == 4 && ip.every(function (segment) {
-    return validateNum(segment, 0, 255);
-  });
-}
-
-function validateNum(input, min, max) {
-  var num = +input;
-  return num >= min && num <= max && input === num.toString();
-}
 
 //  -------------------------
 
@@ -141,303 +83,24 @@ bot.on('ready', async () => {
 
 //  -------------------------
 
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  bot.commands.set(command.name, command);
+}
+
 bot.on('messageCreate', async (message) => {
-  if (message.author.bot || !message.guild) return;
-  let args = message.content.toLowerCase().split(" ");
-  let command = args.shift()
+  if (!message.content.startsWith(PREFIX) || message.author.bot) return
+  const args = message.content.slice(PREFIX.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
 
-  if (command == PREFIX + 'help') {
-    if (NCOMMAND) {
-      let embedss = new Discord.MessageEmbed()
-        .setColor(COLORBOX)
-        .setDescription(`Completed \`${PREFIX}help\``)
-      message.reply({
-        embeds: [embedss]
-      }).then((msg) => {
-        setTimeout(() => {
-          msg.delete();
-          console.log(`Delete notification message ${PREFIX}help`);
-        }, 5000);
-      });
-    }
-    let embed = new Discord.MessageEmbed()
-      .setTitle(`Bot commands list`)
-      .setDescription(`\`\`\`fix
-> ${PREFIX}s <name players> - Search players by name.
-> ${PREFIX}id <number id>   - Search players by number.
-> ${PREFIX}all              - Show all players.
-> ${PREFIX}ip <ip:port>     - Shows status of a given server.
-> ${PREFIX}clear <number>   - Clear all message from bots
-> ${PREFIX}botstop          - Manual stop system.\`\`\``)
-      .setTimestamp()
-      .setColor(COLORBOX)
-      .setFooter({
-        text: `Github: Kuju29/fivem-bots-discord`
-      })
-    message.reply({
-      embeds: [embed]
-    }).then((msg) => {
-      console.log(`Completed ${PREFIX}help`);
-      setTimeout(() => {
-        if (AUTODELETE) {
-          msg.delete();
-          console.log(`Auto delete message ${PREFIX}help`);
-        }
-      }, 10000);
-    });
-  }
-
-  //  -------------------------
-
-  inFo.getPlayers().then(async (players) => {
-
-    if (command == PREFIX + 's') {
-      if (NCOMMAND) {
-        let embedss = new Discord.MessageEmbed()
-          .setColor(COLORBOX)
-          .setDescription(`Completed \`${PREFIX}s\``)
-        message.reply({
-          embeds: [embedss]
-        }).then((msg) => {
-          setTimeout(() => {
-            msg.delete();
-            console.log(`Delete notification message ${PREFIX}s`);
-          }, 5000);
-        });
-      }
-      let text = message.content.toLowerCase().substr(3, 20);
-      let playerdata = players.filter(function (person) {
-        return person.name.toLowerCase().includes(`${text}`)
-      });
-      let result1 = [];
-      let index = 1;
-      for (let player of playerdata) {
-        result1.push(`${index++}. ${player.name} | ID : ${player.id} | Ping : ${player.ping}\n`);
-      };
-      const result = result1.join("\n").toString();
-      let embed = new Discord.MessageEmbed()
-        .setColor(COLORBOX)
-        .setTitle(`Search player | ${SERVER_NAME}`)
-        .setDescription(result.length > 0 ? result : 'No Players')
-        .setTimestamp();
-      message.reply({
-        embeds: [embed]
-      }).then((msg) => {
-        console.log(`Completed ${PREFIX}s ${text}`);
-        setTimeout(() => {
-          if (AUTODELETE) {
-            msg.delete();
-            console.log(`Auto delete message ${PREFIX}s ${text}`);
-          }
-        }, 10000);
-      });
-    }
-
-    if (command == PREFIX + 'id') {
-      if (NCOMMAND) {
-        let embedss = new Discord.MessageEmbed()
-          .setColor(COLORBOX)
-          .setDescription(`Completed \`${PREFIX}id\``)
-        message.reply({
-          embeds: [embedss]
-        }).then((msg) => {
-          setTimeout(() => {
-            msg.delete();
-            console.log(`Delete notification message ${PREFIX}id`);
-          }, 5000);
-        });
-      }
-      let num = message.content.match(/[0-9]/g).join('').valueOf();
-      let playerdata = players.filter(players => players.id == num);
-      let result1 = [];
-      let index = 1;
-      for (let player of playerdata) {
-        result1.push(`${index++}. ${player.name} | ID : ${player.id} | Ping : ${player.ping}\n`);
-      };
-      const result = result1.join("\n").toString();
-      let embed = new Discord.MessageEmbed()
-        .setColor(COLORBOX)
-        .setTitle(`Search player | ${SERVER_NAME}`)
-        .setDescription(result.length > 0 ? result : 'No Players')
-        .setTimestamp();
-      message.reply({
-        embeds: [embed]
-      }).then((msg) => {
-        console.log(`Completed ${PREFIX}id ${num}`);
-        setTimeout(() => {
-          if (AUTODELETE) {
-            msg.delete();
-            console.log(`Auto delete message ${PREFIX}id ${num}`);
-          }
-        }, 10000);
-      });
-    }
-
-    if (command == PREFIX + 'all') {
-      if (NCOMMAND) {
-        let embedss = new Discord.MessageEmbed()
-          .setColor(COLORBOX)
-          .setDescription(`Completed \`${PREFIX}all\``)
-        message.reply({
-          embeds: [embedss]
-        }).then((msg) => {
-          setTimeout(() => {
-            msg.delete();
-            console.log(`Delete notification message ${PREFIX}all`);
-          }, 5000);
-        });
-      }
-      let result = [];
-      let index = 1;
-      for (let player of players) {
-        result.push(`${index++}. ${player.name} | ID : ${player.id} | Ping : ${player.ping}\n`);
-      };
-      let chunks = splitChunks(result.join("\n").toString(), 2000);
-      // let chunks = Discord.Util.splitMessage(result.join("\n"))
-      let embed = new Discord.MessageEmbed().setTitle(`All_players | ${SERVER_NAME}`);
-      if (result.length > 1) {
-        const embeds = chunks.map((chunk) => {
-          return new Discord.MessageEmbed()
-            .setColor(COLORBOX)
-            .setDescription(chunk)
-        });
-        await new Pagination(message.channel, embeds, "Part").paginate();
-        console.log(`Completed !all`);
-      } else {
-        embed.setColor(COLORBOX)
-          .setDescription(result.length > 0 ? result : 'No Players')
-        message.reply({
-          embeds: [embed]
-        }).then((msg) => {
-          console.log(`Completed ${PREFIX}all No Players`);
-          setTimeout(() => {
-            if (AUTODELETE) {
-              msg.delete();
-              console.log(`Auto delete message ${PREFIX}all No Players`);
-            }
-          }, 10000);
-        });
-      }
-    }
-
-  }).catch((err) => {
-    console.log(`Catch ERROR` + err);
-  });
-
-  //  -------------------------
-
-  if (command == PREFIX + 'clear') {
-    if (NCOMMAND) {
-      let embedss = new Discord.MessageEmbed()
-        .setColor(COLORBOX)
-        .setDescription(`Completed \`${PREFIX}clear\``)
-      message.reply({
-        embeds: [embedss]
-      }).then((msg) => {
-        setTimeout(() => {
-          msg.delete();
-          console.log(`Delete notification message ${PREFIX}clear`);
-        }, 5000);
-      });
-    }
-    let num = message.content.match(/[0-9]/g).join('').valueOf();
-    const Channel = message.channel;
-    const Messages = await Channel.messages.fetch({
-      limit: num
-    });
-    Messages.forEach(message => {
-      if (message.author.bot) message.delete()
-    });
-    console.log(`Completed ${PREFIX}Clear ${num}`);
-  }
-
-  if (command == PREFIX + 'ip') {
-    if (NCOMMAND) {
-      let embedss = new Discord.MessageEmbed()
-        .setColor(COLORBOX)
-        .setDescription(`Completed \`${PREFIX}ip\``)
-      message.reply({
-        embeds: [embedss]
-      }).then((msg) => {
-        setTimeout(() => {
-          msg.delete();
-          console.log(`Delete notification message ${PREFIX}ip`);
-        }, 5000);
-      });
-    }
-    let text = message.content.toLowerCase().substr(4, 24);
-    let testip = validateIpAndPort(text);
-    const iNfo = new fivem.ApiFiveM(text);
-    if (testip) {
-      iNfo.checkOnlineStatus().then(async (server) => {
-        if (server !== false) {
-          let infoplayers = (await iNfo.getDynamic());
-          let embed = new Discord.MessageEmbed()
-            .setColor(COLORBOX)
-            .setTitle(`Server: \`${text}\``)
-            .addField('**Server Status**', `\`\`\`✅Online\`\`\``, true)
-            .addField('**Online Players**', `\`\`\`${infoplayers.clients}/${infoplayers.sv_maxclients}\`\`\``, true)
-            .setTimestamp(new Date());
-          message.reply({
-            embeds: [embed]
-          }).then((msg) => {
-            console.log(`Completed ${PREFIX}ip ${text} online`);
-            setTimeout(() => {
-              if (AUTODELETE) {
-                msg.delete();
-                console.log(`Auto delete message ${PREFIX}ip ${text} online`);
-              }
-            }, 10000);
-          });
-        } else {
-          let embed = new Discord.MessageEmbed()
-            .setColor(COLORBOX)
-            .setTitle(`Server: \`${text}\``)
-            .addField('**Server Status**', `\`\`\`❌Offline or Invalid IP\`\`\``, true)
-            .addField('**Online Players**', `\`\`\`-/-\`\`\``, true)
-            .setTimestamp(new Date());
-          message.reply({
-            embeds: [embed]
-          }).then((msg) => {
-            console.log(`Completed ${PREFIX}ip ${text} offline`);
-            setTimeout(() => {
-              if (AUTODELETE) {
-                msg.delete();
-                console.log(`Auto delete message ${PREFIX}ip ${text} offline`);
-              }
-            }, 10000);
-          });
-        }
-      }).catch((err) => {
-        console.log(`Catch ERROR or Offline: ` + err);
-      });
-    } else {
-      let embed = new Discord.MessageEmbed()
-        .setColor(COLORBOX)
-        .addField(`**Are you sure the IP is correct?**`, `\`${text}\``, true)
-        .setTimestamp(new Date());
-      message.reply({
-        embeds: [embed]
-      }).then((msg) => {
-        console.log(`Completed ${PREFIX}ip Check IP: ${text}`);
-        setTimeout(() => {
-          if (AUTODELETE) {
-            msg.delete();
-            console.log(`Auto delete message ${PREFIX}ip Are you sure the IP is correct? ${text}`);
-          }
-        }, 10000);
-      });
-    };
-  }
-
-  if (command == PREFIX + 'botstop') {
-    console.log(`${PREFIX}stopbot - the bot has been stopped.....`);
-    process.exit(0);
+  if (!bot.commands.has(command)) return
+  try {
+    bot.commands.get(command).execute(message, args);
+  } catch (error) {
+    console.error(error);
   }
 
 });
-
-//  -------------------------
 
 bot.login(BOT_TOKEN).then(null).catch(() => {
   console.log('The token you provided is invalided. Please make sure you are using the correct one from https://discord.com/developers/applications!');
