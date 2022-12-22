c163a8f201ba98c814b8a18c4cc946e2da1f8487
★. KuJu -TH- <plan291037@outlook.com>
2022-12-22 10:10:28 +0700

    Update all.js

--- commands/all.js
+++ commands/all.js
@@ -3,5 +3,5 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("all")
-    .setDescription("all players in server"),
+    .setDescription("all players on your server"),
 };
