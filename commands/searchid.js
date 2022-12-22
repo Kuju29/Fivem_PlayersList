0578d3a93b3c640be939af84be390e699c51b1f7
★. KuJu -TH- <plan291037@outlook.com>
2022-12-22 10:10:13 +0700

    Update searchid.js

--- commands/searchid.js
+++ commands/searchid.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("search-id")
-    .setDescription("Search for a list of players by ID.")
+    .setDescription("Search by ID players for a list of players (name, id, ping)")
     .addStringOption((option) =>
       option
         .setName("number")
