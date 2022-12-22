cc424bad0ecc20be25741eaa6c8eb6f712669892
★. KuJu -TH- <plan291037@outlook.com>
2022-12-22 10:09:42 +0700

    Update searchname.js

--- commands/searchname.js
+++ commands/searchname.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("search-name")
-    .setDescription("Search by player name on the server.")
+    .setDescription("Search by player name on your server")
     .addStringOption((option) =>
       option
         .setName("name")
