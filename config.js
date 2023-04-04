de1566089d34a6b25e4bc63c279420a682e22988
★. KuJu -TH- <plan291037@outlook.com>
2023-04-04 19:20:01 +0700

    Update and rename config.json to config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "URL_SERVER": "0.0.0.0:30120",
+    "SERVER_NAME": "Name server",
+    "BOT_TOKEN": "",
+    "SERVER_LOGO": "https://www.catdumb.com/wp-content/uploads/2022/04/2-75.jpg",
+    "COLORBOX": "Blue", //White, Aqua, Green, Blue, Yellow, Purple, LuminousVividPink, Fuchsia, Gold, Orange, Red, Grey, Navy, DarkAqua, DarkGreen, DarkBlue, DarkPurple, DarkVividPink, DarkGold, DarkOrange, DarkRed, DarkGrey, DarkerGrey, LightGrey, DarkNavy, Blurple, Greyple, DarkButNotBlack, NtQuiteBlack, Random
+    "NAMELIST": "police",
+    "UPDATE_TIME": "7000",
+    "Timezone": "th-TH",
+    "Log_update": false,
+};
