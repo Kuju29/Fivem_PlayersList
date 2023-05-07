f289b10b7a9a868d6ee7ccfe6832e7eb67c423f4
★. KuJu -TH- <plan291037@outlook.com>
2023-05-07 19:20:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
 module.exports = {
-    "URL_SERVER": "0.0.0.0:30120",
+    "URL_SERVER": "0.0.0.0:30120", // IP:port
     "URL_CFX": "", // "https://cfx.re/join/xxxx" or "" for use URL_SERVER. Using cfx.re It only contains online player information and server names.
     "SERVER_NAME": "Name server",
-    "BOT_TOKEN": "",
+    "BOT_TOKEN": "", // https://discord.com/developers/applications
     "SERVER_LOGO": "https://www.catdumb.com/wp-content/uploads/2022/04/2-75.jpg",
-    "COLORBOX": "Blue", //White, Aqua, Green, Blue, Yellow, Purple, LuminousVividPink, Fuchsia, Gold, Orange, Red, Grey, Navy, DarkAqua, DarkGreen, DarkBlue, DarkPurple, DarkVividPink, DarkGold, DarkOrange, DarkRed, DarkGrey, DarkerGrey, LightGrey, DarkNavy, Blurple, Greyple, DarkButNotBlack, NtQuiteBlack, Random
-    "NAMELIST": "police",
-    "UPDATE_TIME": "7000",
-    "Timezone": "th-TH",
+    "COLORBOX": "Blue", // White, Aqua, Green, Blue, Yellow, Purple, LuminousVividPink, Fuchsia, Gold, Orange, Red, Grey, Navy, DarkAqua, DarkGreen, DarkBlue, DarkPurple, DarkVividPink, DarkGold, DarkOrange, DarkRed, DarkGrey, DarkerGrey, LightGrey, DarkNavy, Blurple, Greyple, DarkButNotBlack, NtQuiteBlack, Random
+    "NAMELIST": "police", // name for count
+    "UPDATE_TIME": "7000", // Delay
+    "Timezone": "th-TH", // time zone
+    "STATUS": "online", // "online" | Online, "idle" | Idle, "dnd" | Do Not Disturb, "invisible" | Offline 
     "Log_update": true // true, false
 };
